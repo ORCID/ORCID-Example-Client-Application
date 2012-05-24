af43d1e0aca28d39bb5956282f89082dcd8e1189
Will Simpson <will@semantico.com>
2012-05-24 12:05:42 +0100

    Configured SSL to work with unsigned server certificate in ORCID (for test purposes).

---
+++ src/main/java/org/orcid/examples/jopmts/impl/SSLConfig.java
@@ -0,0 +1,58 @@
+package org.orcid.examples.jopmts.impl;
+
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+import org.springframework.beans.factory.InitializingBean;
+
+/**
+ * 
+ * @author Will Simpson
+ * 
+ */
+public class SSLConfig implements InitializingBean {
+
+    public static void trustSelfSignedSSL() {
+        try {
+            SSLContext ctx = SSLContext.getInstance("TLS");
+            X509TrustManager tm = new X509TrustManager() {
+
+                public void checkClientTrusted(X509Certificate[] xcs, String string) throws CertificateException {
+                }
+
+                public void checkServerTrusted(X509Certificate[] xcs, String string) throws CertificateException {
+                }
+
+                public X509Certificate[] getAcceptedIssuers() {
+                    return null;
+                }
+            };
+            ctx.init(null, new TrustManager[] { tm }, null);
+            SSLContext.setDefault(ctx);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        trustSelfSignedSSL();
+
+        HostnameVerifier hv = new HostnameVerifier() {
+            public boolean verify(String urlHostName, SSLSession session) {
+                System.out.println("Warning: URL Host: " + urlHostName + " vs. " + session.getPeerHost());
+                return true;
+            }
+
+        };
+        HttpsURLConnection.setDefaultHostnameVerifier(hv);
+    }
+
+}
