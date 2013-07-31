9661a43b8b39a7923b6e460f9cf774d4001c6d32
Will Simpson <will@semantico.com>
2013-07-31 12:08:10 +0100

    Upgraded spring-security-oauth2 version 1.0.0.M4 to 1.0.5.RELEASE.

--- src/main/java/org/orcid/examples/jopmts/impl/OrcidServiceImpl.java
+++ src/main/java/org/orcid/examples/jopmts/impl/OrcidServiceImpl.java
@@ -23,23 +23,14 @@
  */
 package org.orcid.examples.jopmts.impl;
 
-import java.net.URI;
 import java.util.Map;
 
 import javax.xml.transform.dom.DOMSource;
 
 import org.orcid.examples.jopmts.OrcidException;
 import org.orcid.examples.jopmts.OrcidService;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.client.OAuth2AccessTokenRequiredException;
-import org.springframework.security.oauth2.client.OAuth2ProtectedResourceDetails;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
-import org.springframework.security.oauth2.client.OAuth2SecurityContext;
-import org.springframework.security.oauth2.client.OAuth2SecurityContextHolder;
-import org.springframework.security.oauth2.client.OAuth2SecurityContextImpl;
-import org.springframework.security.oauth2.client.token.OAuth2ClientTokenServices;
-import org.springframework.security.oauth2.common.exceptions.InvalidTokenException;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.w3c.dom.Document;
 
 /**
@@ -49,19 +40,18 @@ public class OrcidServiceImpl implements OrcidService {
     private String orcidInfoURL;
     private String orcidSearchURL;
     private OAuth2RestTemplate orcidRestTemplate;
-    private OAuth2ClientTokenServices tokenServices;
 
     public Document getOrcidDocument() throws OrcidException {
         return getOrcidDocument(null);
     }
 
     public Document getOrcidDocument(String orcid) throws OrcidException {
-        AuthenticatedResource authRes = new AuthenticatedResource(orcidRestTemplate, tokenServices);
         if (orcid == null) {
-            orcid = authRes.getToken().getOrcid();
+            OAuth2AccessToken accessToken = orcidRestTemplate.getAccessToken();
+            orcid = (String) accessToken.getAdditionalInformation().get("orcid");
         }
         String url = String.format(orcidInfoURL, orcid);
-        return authRes.fetchDocument(url);
+        return (Document) orcidRestTemplate.getForObject(url, DOMSource.class).getNode();
     }
 
     public Document searchOrcid(Map<String, String> searchTerms) throws OrcidException {
@@ -76,9 +66,8 @@ public class OrcidServiceImpl implements OrcidService {
     }
 
     public Document searchOrcid(String query) throws OrcidException {
-        AuthenticatedResource authRes = new AuthenticatedResource(orcidRestTemplate, tokenServices);
         String url = orcidSearchURL + "?q=" + query;
-        return authRes.fetchDocument(url);
+        return orcidRestTemplate.getForObject(url, Document.class);
     }
 
     public void setOrcidInfoURL(String infoURL) {
@@ -89,67 +78,8 @@ public class OrcidServiceImpl implements OrcidService {
         this.orcidSearchURL = infoURL;
     }
 
-    public void setOrcidRestTemplate(OAuth2RestTemplate template) {
-        this.orcidRestTemplate = template;
-    }
-
-    public void setTokenServices(OAuth2ClientTokenServices tokenServices) {
-        this.tokenServices = tokenServices;
-    }
-
-    public static class AuthenticatedResource {
-        private Authentication authentication;
-        private OAuth2ProtectedResourceDetails resource;
-        private OrcidOAuth2AccessToken token;
-        private OAuth2RestTemplate orcidRestTemplate;
-        private OAuth2ClientTokenServices tokenServices;
-
-        public AuthenticatedResource(OAuth2RestTemplate orcidRestTemplate, OAuth2ClientTokenServices tokenServices) {
-            this.orcidRestTemplate = orcidRestTemplate;
-            this.tokenServices = tokenServices;
-            this.authentication = SecurityContextHolder.getContext().getAuthentication();
-            this.resource = orcidRestTemplate.getResource();
-            this.token = (OrcidOAuth2AccessToken) tokenServices.getToken(authentication, resource);
-            if (token == null) {
-                // go get an access token...
-                throw new OAuth2AccessTokenRequiredException(resource);
-            }
-        }
-
-        public Document fetchDocument(String url) {
-            System.err.println("Fetching URL: " + url);
-            try {
-                DOMSource orcidInput = orcidRestTemplate.getForObject(URI.create(url), DOMSource.class);
-                return (Document) orcidInput.getNode();
-            } catch (InvalidTokenException badToken) {
-                // we've got a bad token, probably because it's expired.
-                // clear any stored access tokens...
-                tokenServices.removeToken(authentication, resource);
-                // go get a new access token...
-                throw new OAuth2AccessTokenRequiredException(resource);
-            } finally {
-                clearAuthorizationCode(authentication, resource);
-            }
-        }
-
-        private void clearAuthorizationCode(Authentication authentication, OAuth2ProtectedResourceDetails resource) {
-            OAuth2SecurityContext oauthContext = OAuth2SecurityContextHolder.getContext();
-            if (oauthContext != null) {
-                ((OAuth2SecurityContextImpl) oauthContext).setAuthorizationCode(null);
-            }
-        }
-
-        public Authentication getAuthentication() {
-            return authentication;
-        }
-
-        public OAuth2ProtectedResourceDetails getResource() {
-            return resource;
-        }
-
-        public OrcidOAuth2AccessToken getToken() {
-            return token;
-        }
+    public void setOrcidRestTemplate(OAuth2RestTemplate orcidRestTemplate) {
+        this.orcidRestTemplate = orcidRestTemplate;
     }
 
 }
